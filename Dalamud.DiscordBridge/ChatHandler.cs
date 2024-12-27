26d77f9229c7116ae50f17fd2c562b9c3ff23e7f
Raiden <153776281+RaidenvBlack@users.noreply.github.com>
2024-12-27 14:42:28 +0100

    hopefully all in one

---
+++ Dalamud.DiscordBridge/ChatHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Threading.Tasks;
+using Dalamud.DiscordBridge.Model;
+using Dalamud.Game.Text;
+using Dalamud.Plugin.Services;
+using Discord;
+using Discord.Webhook;
+using Discord.WebSocket;
+using Lumina.Text.ReadOnly;
+using NetStone;
+using NetStone.Model.Parseables.Character;
+using NetStone.Search.Character;
+namespace Dalamud.DiscordBridge
+{
+public class ChatHandler
+{
+   static readonly IPluginLog Logger = Service.Logger;
+   public void HandleMessage(string channel, string message)
+   {
+      Logger.Error($"[ChatHandler] Channel: {channel}, Message: {message}");
+   }
+}
+}
